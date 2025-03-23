8555e4183af2c3409c181799a6345d62574d9a66
sp33dfre4k <sp33dfre4k@proton.me>
2025-03-23 18:12:00 -0400

    Add constants

---
+++ programs/bullrider/src/constants.rs
@@ -0,0 +1,6 @@
+// Transfer fee constants
+pub const TRANSFER_FEE_BASIS_POINTS: u16 = 2500; // 25%
+pub const MAXIMUM_FEE: u64 = u64::MAX; // No maximum fee limit
+
+// Interest rate constants
+pub const INTEREST_RATE_BASIS_POINTS: u16 = 700; // 7% interest 
