0f185d0a06897fa75bc12586401349056c11eebf
sp33dfre4k <sp33dfre4k@proton.me>
2025-03-23 18:20:57 -0400

    Add lib entrypoint and update features to fix cfg compiler warnings

--- programs/bullrider/src/lib.rs
+++ programs/bullrider/src/lib.rs
@@ -1,16 +1,23 @@
 use anchor_lang::prelude::*;
 
-declare_id!("8oCRvRy6ScwN1JX9QvJsmS9UrcdqiGpcBTZLBDhX5vHb");
+mod constants;
+mod instructions;
+mod state;
+
+declare_id!("2i8dyR8UNhP5GtiRrrUX8eZiMWNRDdzVg5SxZ58Bj6Wp");
 
 #[program]
 pub mod bullrider {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+        instructions::initialize::handler(ctx)
+    }
+    
+    pub fn mint_token(ctx: Context<MintToken>, amount: u64) -> Result<()> {
+        instructions::mint_token::handler(ctx, amount)
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+// Re-export account structures
+pub use state::*;
