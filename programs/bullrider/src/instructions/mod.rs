07a9b593bced43aeb4beb3e7ad25af68b113f277
sp33dfre4k <sp33dfre4k@proton.me>
2025-03-26 23:11:41 -0400

    Fix compiler errors

--- programs/bullrider/src/instructions/mod.rs
+++ programs/bullrider/src/instructions/mod.rs
@@ -1,2 +1,5 @@
-pub mod withdraw_and_burn;
-pub mod claim_interest;
+pub mod withdraw_and_burn_ix;
+pub mod claim_interest_ix;
+
+pub use withdraw_and_burn_ix::WithdrawAndBurn;
+pub use claim_interest_ix::ClaimInterest;
--- programs/bullrider/src/lib.rs
+++ programs/bullrider/src/lib.rs
@@ -1,18 +1,19 @@
 use anchor_lang::prelude::*;
 
 pub mod instructions;
+use instructions::{WithdrawAndBurn, ClaimInterest};
 
 declare_id!("8oCRvRy6ScwN1JX9QvJsmS9UrcdqiGpcBTZLBDhX5vHb");
 
 #[program]
-pub mod bullride {
+pub mod bullrider {
     use super::*;
 
     pub fn withdraw_and_burn(ctx: Context<WithdrawAndBurn>) -> Result<()> {
-        instructions::withdraw_and_burn::handler(ctx)
+        instructions::withdraw_and_burn_ix::handler(ctx)
     }
 
     pub fn claim_interest(ctx: Context<ClaimInterest>, interest_amount: u64) -> Result<()> {
-        instructions::claim_interest::handler(ctx, interest_amount)
+        instructions::claim_interest_ix::handler(ctx, interest_amount)
     }
 }
