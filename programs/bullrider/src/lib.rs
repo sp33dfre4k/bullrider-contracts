85c47a03a4272f2e95299632577fc433ba61f1e4
sp33dfre4k <sp33dfre4k@proton.me>
2025-03-28 15:17:00 -0400

    Delete withdraw and burn from lib

--- programs/bullrider/src/lib.rs
+++ programs/bullrider/src/lib.rs
@@ -9,10 +9,6 @@ declare_id!("8oCRvRy6ScwN1JX9QvJsmS9UrcdqiGpcBTZLBDhX5vHb");
 pub mod bullrider {
     use super::*;
 
-    pub fn withdraw_and_burn(ctx: Context<WithdrawAndBurn>) -> Result<()> {
-        instructions::withdraw_and_burn_ix::handler(ctx)
-    }
-
     pub fn claim_interest(ctx: Context<ClaimInterest>) -> Result<()> {
         instructions::claim_interest_ix::handler(ctx)
     }
